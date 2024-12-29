5901cebeb9da03a8d65d7a7a0fefe7065578d317
NajeebullahSadiq <najeebullahsadiq590@gmail.com>
2024-12-29 22:00:19 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Najib app</title>
+</head>
+<body>
+    <h1 class="header">This will be the header</h1>
+    <a href="/home">The Home page</a>
+</body>
+</html>
